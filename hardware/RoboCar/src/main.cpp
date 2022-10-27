19ad540377c66913d53405227ff92252a6b0dd00
MikhailDeriabin <mikko.deriabin@gmail.com>
2022-10-27 16:56:51 +0300

    add esp commands manipulation functions

--- hardware/RoboCar/src/main.cpp
+++ hardware/RoboCar/src/main.cpp
@@ -5,6 +5,9 @@
 #include <USSensor.h>
 #include <PhotoResistor.h>
 #include <Component.h>
+#include <Converter.h>
+
+Converter converter;
 
 const int FLMotorEnablePin = 33;
 const int FLMotorInput1Pin = 31;
@@ -52,14 +55,47 @@ void setup() {
   setComponentsIds(components, componentsCount);
 }
 
+const int bufferSize = 50;
+char serialBuffer[bufferSize];
+int serialBufferIndex = 0;
+
 void loop() {
   if(Serial3.available() > 0) {
-    char incomingByte = (char)Serial3.read();
-
+    char incomingChar = (char)Serial3.read();
+    if(incomingChar == ';'){
+      clearCharArray(serialBuffer, bufferSize);
+      serialBufferIndex = 0;
+    } else{
+      serialBuffer[serialBufferIndex] = incomingChar;
+      serialBufferIndex++;
+    }
   }
 }
 
 void setComponentsIds(Component* components[], int componentsSize){
   for(int i=0; i<componentsSize; i++)
     components[i]->setId(i); 
+}
+
+void clearCharArray(char arr[], int arrSize){
+  for(int i=0; i<arrSize; i++){
+    arr[i] = -1;
+  }
+}
+
+void sendCommand(char str[], int strSize){
+  char idRaw[10];
+  int deviceId = -1;
+  int i = 0;
+  for(; i<strSize; i++){   
+    if(str[i] == '_'){
+      deviceId = converter.charArrToInt(idRaw, i-1);
+      break;
+    }
+    idRaw[i] = str[i];
+  }
+
+  
+
+
 }
