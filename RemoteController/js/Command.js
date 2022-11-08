97af552af989790205cef8ef5e2d5985cfe0f5f6
Mikhail <mikko.deriabin@gmail.com>
2022-11-08 19:38:53 +0200

    add remote controller for robot on vanilla js

---
+++ RemoteController/js/Command.js
@@ -0,0 +1,25 @@
+export const Command = {
+    OFF : 0,
+    ON: 1,
+
+    PULSE: 2,
+    SET_BRIGHTNESS: 3,
+    SET_INTENSIVITY: 4,
+    SET_COLOR: 5,
+
+    SPIN_CLOCKWISE: 6,
+    SPIN_COUNTERCLOCKWISE: 7,
+    CHANGE_SPIN_DIRECTION: 8,
+    STOP_SPIN: 9,
+
+    TURN_ON_ANGLE: 10,
+
+    MOVE_FORWARD: 11,
+    MOVE_BACK: 12,
+    TURN_LEFT: 13,
+    TURN_RIGHT: 14,
+    STOP_MOVING: 15,
+    GET_COORDINATES: 16,
+
+    MEASURE: 17
+};
---
+++ RemoteController/js/SensorValue.js
@@ -0,0 +1,13 @@
+export const SensorValue = {
+    NOT_SPECIFIED : 0,
+
+    TEMPERATURE: 1,
+    HUMIDITY: 2,
+    LIGHT_INTENSITY: 3,
+    IS_TILTED: 4,
+
+    DISTANCE: 5,
+    DISTANCE_FRONT: 6,
+    DISTANCE_LEFT: 7,
+    DISTANCE_RIGHT: 8
+};
